c1fc4e07ac292c67acd1f8d5f19329791f28a50b
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2025-11-25 13:04:34 +0000

    :skull:tokenizer project skeleton

---
+++ codellm_tokenizer/README.md
@@ -0,0 +1,3 @@
+# Tokenizer
+
+Write up: https://dudeperf3ct.github.io/projects/train_llm_part1/
---
+++ codellm_tokenizer/main.py
@@ -0,0 +1,10 @@
+"""Main module."""
+
+
+def main():
+    """Entrypoint."""
+    print("Hello from codellm-tokenizer!")
+
+
+if __name__ == "__main__":
+    main()
